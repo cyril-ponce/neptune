226edfa5bf51c6e3b94c5609786db1fb33d534c5
Austin Walters <lettergramm@gmail.com>
2014-05-26 15:00:00 -0500

    Update bkz.go

--- pkgs/bkz/bkz.go
+++ pkgs/bkz/bkz.go
@@ -21,21 +21,27 @@ type Book struct {
 // Creates an account and adds it to the Database
 func CreateBook(book *Book) bool {
 
+	// Dial up a mongoDB session
 	session, err := mgo.Dial("127.0.0.1:27017/")
 	if err != nil {
 		fmt.Println(err)
 		return false
 	}
 
-	c := session.DB("library").C("users")
+	// Opens the "library" databases, "books" collection
+	c := session.DB("library").C("books")
 	result := Book{}
+	
+	// Search for the bookID, place in result.Id
 	err = c.Find(bson.M{"id": book.Id}).One(&result)
+	
 	if result.Id != "" {
 		// return true because book is present in the database
 		// and we can say, "it's been added" without causing errors
 		return true
 	}
 
+	// insert the book if it is not already in the database
 	err = c.Insert(*book)
 
 	if err != nil {
@@ -44,6 +50,7 @@ func CreateBook(book *Book) bool {
 	return true
 }
 
+// Finds book already in database
 func FindBook(bookid string) (book *Book) {
 
 	session, err := mgo.Dial("127.0.0.1:27017/")
@@ -52,10 +59,11 @@ func FindBook(bookid string) (book *Book) {
 		return nil
 	}
 	
-	// Should probably change "users" to "books"
-	c := session.DB("library").C("users")
+	// Opens the "library" databases, "books" collection
+	c := session.DB("library").C("books")
+	
+	// Finds the bookid and fills the book struct with the data
 	err = c.Find(bson.M{"id": bookid}).One(&book)
 
 	return book
-
 }
